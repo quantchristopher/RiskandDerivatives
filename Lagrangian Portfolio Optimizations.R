02ba27c0bd2a02f4a23b84fc96fc43dc92080b17
quantchristopher <142529448+quantchristopher@users.noreply.github.com>
2023-08-21 11:50:35 -0600

    Update Lagrangian Portfolio Optimizations.R
    Updates to comments

--- Lagrangian Portfolio Optimizations.R
+++ Lagrangian Portfolio Optimizations.R
@@ -1,13 +1,19 @@
-##### Portfolio Optimization for given return and risk tolerances
+##### Portfolio Optimization for given return and risk tolerances by Christopher Thomson
 
-## The following code fulfills the task of a Markowitz portolio optimization for 
-## n - risky assets using Lagrangian Multipliers and matrix method solutions. 
+## The following code fulfills the task of a Markowitz portolio optimization problem for 
+## n - risky assets. In other words, the goal is to efficiently allocate funds in a portfolio 
+## to a number of assets with fully known return distributions and covariance/correlation relationships.
+## By "efficiently" we mean to minimize the covariance/correlation risk of the portfolio while seeking an 
+## a predetermined expected return on the entire portfolio. The only constraint on that must be maintained 
+## is the "initial capital for investment" must be fully utilized and all available stocks must recieve allocation. 
+## (however very very small and/or very very large allocations are allowed AND negative or shortselling is allowed). 
+## The process is facilitated using Lagrangian Multipliers for matrix method solutions. 
 
 
 ### Function Setup #### 
-## Markowitz portfolio optimization of w_vec, given constraint that all of
-## w_vec must be invested, portfolio covariance matrix must be minimized
 
+## The function incorporates three variables: 1. The mean of returns vector - mu 2. The known covariance
+## matrix - cv 3. The expected or "required" return on the aggregate portfolio - Er
 markowitz <- function(mu,cv,Er) {
   n = length(mu)
   wuns = matrix(1,n,1)
@@ -24,22 +30,18 @@ markowitz <- function(mu,cv,Er) {
   wts = g + h*Er
 }
 
+## An example of the above:
 mu = matrix(c(0.02,0.10,0.20),3,1) 
 n = length(mu)
 cv = matrix(c(0.0001,0,0,0,0.04,0.02,0,0.02,0.16),n,n)
 Er = 0.18
-
-wts = markowitz(mu,cv,Er)
+wts = markowitz(mu,cv,Er) # Store the output of the function
 print(wts)
-### Note the output of wts under these parameters requires that the first asset/mu[1] has
-## to be sold short to minimized the variance
-
-### Quadprog Package can also be used to conduct these optimization problems with the added
-## benefit of including additional constraints such as inequalities such as no-shortselling,
-## minimum variance must still be above a certain threshold etc.
+# Note the output of wts under these parameters requires that the first asset/mu[1] has
+# to be sold short to minimized the variance.
 
 
-### Tracing the efficient frontier by toggling E[rp]
+### Tracing the efficient frontier of the portfolio for various return to risk tolerances by toggling E[rp]:
 Er_vec <- matrix(seq(0.01,0.15,0.01),15,1)
 sig_vec <- matrix(0,15,1)
 j = 0
@@ -48,8 +50,9 @@ for (Er in Er_vec) {
   wts = markowitz(mu,cv,Er)
   sig_vec[j] = sqrt(t(wts)%*%cv%*%wts)
 }
-### For given mu values of each component, the above code calculates the different weight
-## allocations required to give an expected return on a portfolio and the associated standard
-## deviation. 
- 
+# For given mu values of each component, the above code calculates the different weight
+# allocations required to give an expected return on a portfolio and the associated standard
+# deviation. 
+
+## Plotting the efficient frontier:
 plot(sig_vec, Er_vec, type = "l", main = "Efficient Frontier of Portfolios")
